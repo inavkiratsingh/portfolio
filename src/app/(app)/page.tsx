7e2caec865bca66de2d550b9dead0aebf1251cd1
Ubuntu <navs6164@gmail.com>
2024-12-01 12:02:33 +0530

    error resolvment

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,6 @@
-'use client'
 import PasskeyModal from "@/components/PasskeyModal";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 
 export default function page() {  
@@ -12,26 +10,25 @@ export default function page() {
   return (
 
     
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="px-20 pt-14">
-        {isAdmin && <PasskeyModal />}
-        <h1 className="text-6xl uppercase tracking-widest text-zinc-100">I'm Navkirat Singh</h1>
-        <p className="text-zinc-400 py-6 leading-8">
-          
-          Your friendly neighborhood frontend developer, UX architect, and
-          JavaScript engineer. I spend my days (and often nights) painting the
-          Internet canvas with Projects and lines of code, turning zeroes and ones
-          into immersive, interactive experiences, <br /><br />Bona fide photochromic Lens
-          enthusiast - sunlight or indoors, I've got it covered. I tread the path
-          of minimalism, finding beauty in simplicity and order. When I'm not
-          crafting beautiful web experiences, you can find me reading Articles or
-          swaying to the rhythm of Pop Music & Jazz, losing myself in the
-          captivating flow of melodies. anyways you can Contact Me
-        </p>
-        <Link href="#" className="text-zinc-400">
-          <div className="pt-7">See More About Me</div>
-        </Link>
-      </div>
-    </Suspense>
+    
+    <div className="px-20 pt-14">
+      {isAdmin && <PasskeyModal />}
+      <h1 className="text-6xl uppercase tracking-widest text-zinc-100">I'm Navkirat Singh</h1>
+      <p className="text-zinc-400 py-6 leading-8">
+        
+        Your friendly neighborhood frontend developer, UX architect, and
+        JavaScript engineer. I spend my days (and often nights) painting the
+        Internet canvas with Projects and lines of code, turning zeroes and ones
+        into immersive, interactive experiences, <br /><br />Bona fide photochromic Lens
+        enthusiast - sunlight or indoors, I've got it covered. I tread the path
+        of minimalism, finding beauty in simplicity and order. When I'm not
+        crafting beautiful web experiences, you can find me reading Articles or
+        swaying to the rhythm of Pop Music & Jazz, losing myself in the
+        captivating flow of melodies. anyways you can Contact Me
+      </p>
+      <Link href="#" className="text-zinc-400">
+        <div className="pt-7">See More About Me</div>
+      </Link>
+    </div>
   );
 }
